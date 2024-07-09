c8b0378d9f62654315b72d4e9ba6886eb34dce30
RueianSu <82689198+RueianSu@users.noreply.github.com>
2024-07-09 15:26:45 +0800

    加入專案檔案。

---
+++ Seasons/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seasons
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*輸入月份1~12月，利用switch判斷相對應的季節春、夏、秋、冬並印出。若不在此範圍則印出”輸入錯誤”。*/
+            int month;
+            Console.WriteLine("利用switch判斷相對應的季節春、夏、秋、冬");
+            Console.WriteLine("請輸入月份");
+
+            var monthStr = Console.ReadLine();
+            month = !IsInteger(monthStr) ? 99 : int.Parse(monthStr);
+
+            Console.WriteLine((month > 12 || month <= 0) ? "輸入錯誤" : MonthLine(month));
+            Console.Read();
+        }
+
+        static string MonthLine(int month)
+        {
+            int result = Convert.ToInt16(Math.Ceiling(month / 3f) - 1);
+
+            switch (result)
+            {
+                case 0:
+                    return "春";
+                case 1:
+                    return "夏";
+                case 2:
+                    return "秋";
+                case 3:
+                    return "冬";
+            }
+            return "錯誤";
+        }
+        static bool IsInteger(string input)
+        {
+            return int.TryParse(input, out _);
+        }
+    }
+}
